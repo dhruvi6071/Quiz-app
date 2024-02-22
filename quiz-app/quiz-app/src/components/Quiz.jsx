22aa6003040498af95f46602a81827a8019b2704
dhruvi6071 <dhruvi6071@gmail.com>
2024-02-22 19:54:43 +0530

    quiz-complete-crash-solve

--- quiz-app/quiz-app/src/components/Quiz.jsx
+++ quiz-app/quiz-app/src/components/Quiz.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import QUESTIONS from '../questions.js';
+import image from '../assets/quiz-complete.png';
 
 export default function Quiz() {
 // const[currentQuestion, setCurrentQuestion] = useState(0); //To manage the data which questions are displayed
 // we can simply derive currentQuestion index using number of elements inserted into array of givenAnswers. therefore above line is not needed.   
 
 const[givenAnswer, setGivenAnswer] = useState([]); // To keep track for given answers from the user.
-// const currentQuestionIndex = givenAnswer.length;
-const shuffleAnswer = [...QUESTIONS[currentQuestionIndex].answers];
-shuffleAnswer.sort(() => Math.random() - 0.5);
+const currentQuestionIndex = givenAnswer.length;
 const quizIsComplete = currentQuestionIndex === QUESTIONS.length; // To make sure that number of questions does not exceed the limit. (On finish point it finishes smoothly without causing the site break) 
 
 
@@ -17,6 +16,19 @@ function handleSelectAnswer(selectedAnswer) {
         return [...prevUserAnswers, selectedAnswer];
     });
 }
+
+if(quizIsComplete){
+    return (<div id="summary">
+        <img src={image} alt="Winning-trophy-image"/>
+        <h2>Quiz Completed!</h2>
+    </div>
+    );
+}
+
+// const currentQuestionIndex = givenAnswer.length;
+const shuffleAnswer = [...QUESTIONS[currentQuestionIndex].answers];
+shuffleAnswer.sort(() => Math.random() - 0.5);
+
    return (
     <div id="quiz">
     <div id="questions">
